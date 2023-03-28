5ea983aa47e105d174ff52404a09c4e111c54a15
amilewska <adoryanna@gmail.com>
2023-03-28 19:58:59 +0200

    enemy, collisions

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            Debug.Log("Player detected - ATTACT!");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            Debug.Log("Player out of range, resume patrol!");
+        }
+    }
+
+}
--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -10,12 +10,30 @@ public class PlayerBehaviour : MonoBehaviour
     private float verticalInput;
     private float horizontalInput;
 
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     private void Update()
     {
         verticalInput = Input.GetAxis("Vertical") * moveSpeed;
         horizontalInput = Input.GetAxis("Horizontal") * rotateSpeed;
 
-        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime);
-        transform.Rotate(Vector3.up * horizontalInput * Time.deltaTime);
+        /*transform.Translate(Vector3.forward * verticalInput * Time.deltaTime);
+        transform.Rotate(Vector3.up * horizontalInput * Time.deltaTime);*/
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 rotation = Vector3.up * horizontalInput;
+        Quaternion angleRotation = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+
+        rb.MovePosition(transform.position + transform.forward * verticalInput * Time.fixedDeltaTime);
+        rb.MoveRotation(rb.rotation * angleRotation);
     }
 }
