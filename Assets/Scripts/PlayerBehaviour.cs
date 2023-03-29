6e73aec656cfaa08d81862edda20f80f570be153
amilewska <adoryanna@gmail.com>
2023-03-29 11:42:30 +0200

    ground check

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -12,9 +12,17 @@ public class PlayerBehaviour : MonoBehaviour
 
     private Rigidbody rb;
 
+    [SerializeField] private float jumpVelocity = 5f;
+    private bool isJumping;
+
+    [SerializeField] float distanceToGround = 0.1f;
+    [SerializeField] private LayerMask groundLayer;
+    private CapsuleCollider col;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
     }
 
     private void Update()
@@ -24,6 +32,8 @@ public class PlayerBehaviour : MonoBehaviour
 
         /*transform.Translate(Vector3.forward * verticalInput * Time.deltaTime);
         transform.Rotate(Vector3.up * horizontalInput * Time.deltaTime);*/
+
+        isJumping |= Input.GetKeyDown(KeyCode.Space);
 
 
     }
@@ -35,5 +45,21 @@ public class PlayerBehaviour : MonoBehaviour
 
         rb.MovePosition(transform.position + transform.forward * verticalInput * Time.fixedDeltaTime);
         rb.MoveRotation(rb.rotation * angleRotation);
+
+        if(IsGrounded() && isJumping)
+        {
+            rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+        }
+
+
+    }
+
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottom = new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z);
+
+        bool grounded = Physics.CheckCapsule(col.bounds.center, capsuleBottom, distanceToGround, groundLayer, QueryTriggerInteraction.Ignore);
+
+        return grounded;
     }
 }
